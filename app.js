c5a3180c6912eaea0a53e1ff9e9ad9cf9c213644
dakotas17 <dakotas17@nycstudents.net>
2023-10-11 09:24:22 -0400

    uhoh

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("connected");
+
+const arr = [3,6,9]
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+const arr2 = [true, false, "yay!", [3, 6, 7], null, 9];
+//console.log(arr2.length)
+
+arr2.push("pushed");
+arr2.pop()
+console.log(arr2);
+
+arr2.forEach((numbers) => console.log(numbers));
